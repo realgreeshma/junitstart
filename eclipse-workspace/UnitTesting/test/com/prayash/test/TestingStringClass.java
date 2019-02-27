08700d5a73c01bc0009814434143702af5d53582
realgreeshma <greeshmahere@gmail.com>
2019-02-27 11:41:31 -0600

    Wrting AfterAll and BeforeAll annotations

--- eclipse-workspace/UnitTesting/test/com/prayash/test/TestingStringClass.java
+++ eclipse-workspace/UnitTesting/test/com/prayash/test/TestingStringClass.java
@@ -2,16 +2,49 @@ package com.prayash.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 class TestingStringClass {
+	
+	@BeforeAll
+	static void beforeAll()
+	{
+		System.out.println("Some things which are commom to all tests like connecting to database");
+	}
+
+	@AfterAll
+	static void afterAll()
+	{
+		System.out.println("runs after all tests,Some things which are commom to all tests like dis-connecting to database");
+	}
+	
+	
+	@BeforeEach
+	//void beforeEachTest() { ---without a parameter
+		void beforeEachTest(TestInfo info) {
+		System.out.println("Before each test  " + info.getDisplayName());
+	}
 
 	@Test
-		void split_method_test()
-		{
-			String str = "Pra,Gree,Pra,Yas";
-			String sActual[] = str.split(",");	
-			assertArrayEquals(new String[] {"Pra","Gree","Pra","Yas"},sActual);
-			
-		}
+	void split_method_test() {
+		String str = "Pra,Gree,Pra,Yas";
+		String sActual[] = str.split(",");
+		assertArrayEquals(new String[] { "Pra", "Gree", "Pra", "Yas" }, sActual);
+
 	}
+
+	@Test
+	void math_max_test() {
+		assertEquals(101, Math.max(101, 21));
+	}
+	
+	@AfterEach
+	void AfterEachTest() {
+		System.out.println("After each test");
+	}
+}
